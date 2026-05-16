e42b78d0a63c88949d90c3d10e08bf80f47bd820
Dagmawi Mitiku <mitikudagmawi0@gmail.com>
2026-05-16 20:46:38 +0300

    added logic to the draw rectangle function

--- Flag/main.cpp
+++ Flag/main.cpp
@@ -4,7 +4,13 @@
 #define PI 3.14159265
 
 void drawRectangle(float x, float y, float w, float h, float r, float g, float b) {
-
+    glColor3f(r, g, b);
+    glBegin(GL_QUADS);
+        glVertex2f(x, y);
+        glVertex2f(x + w, y);
+        glVertex2f(x + w, y + h);
+        glVertex2f(x, y + h);
+    glEnd();
 }
 
 int main(int argc, char** argv) {
