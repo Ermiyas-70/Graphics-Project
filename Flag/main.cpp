5051aad0dde94233127917d0bb9afe8244430450
Ermiyas-70 <ermiyastomas70@gmail.com>
2026-05-17 11:01:23 +0300

    Setup baseline for Cook Islands flag collaboration

--- Flag/main.cpp
+++ Flag/main.cpp
@@ -13,16 +13,51 @@ void drawRectangle(float x, float y, float w, float h, float r, float g, float b
     glEnd();
 }
 
+void drawStar(float cx, float cy, float outerR, float innerR) {
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_TRIANGLE_FAN);
+        glVertex2f(cx, cy);
+        for (int i = 0; i <= 10; i++) {
+            float angle = PI / 2 + i * PI / 5;
+            float radius = (i % 2 == 0) ? outerR : innerR;
+            glVertex2f(cx + cos(angle) * radius, cy + sin(angle) * radius);
+        }
+    glEnd();
+}
+
+void drawStudent1_Background() {
+
+}
+
+void drawStudent2_UnionJack() {
+
+}
+
+void drawStudent3_StarsRing() {
+
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    glFlush();
+}
+
+void init() {
+    glClearColor(1, 1, 1, 1);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-1.0, 1.5, -0.75, 0.75);
+}
+
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-
-
     glutInitWindowSize(1000, 600);
-    glutCreateWindow("Cook Islands Flag - Full Screen");
+    glutCreateWindow("Cook Islands Flag - Multi-Student Collaboration");
     init();
-    glutDisplayFunc(drawRectangle);
+    glutDisplayFunc(display);
     glutMainLoop();
     return 0;
 }
-
