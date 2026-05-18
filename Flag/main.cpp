988f80e73817e38d89bfe80ff2bc4c6979709caa
Endu Dev <endawekebirhanu20@gmail.com>
2026-05-18 10:42:26 +0300

    This is the final comit any one don't touch any think

--- Flag/main.cpp
+++ Flag/main.cpp
@@ -3,6 +3,8 @@
 
 #define PI 3.14159265
 
+float waveTime = 0.0f;
+
 void drawRectangle(float x, float y, float w, float h, float r, float g, float b) {
     glColor3f(r, g, b);
     glBegin(GL_QUADS);
@@ -44,10 +46,35 @@ void drawUnionJack() {
     drawRectangle(-1.0, 0.32, 1.0, 0.11, 1, 0, 0);
     drawRectangle(-0.555, 0.0, 0.11, 0.75, 1, 0, 0);
 }
-
+void timer(int value) {
+    waveTime += 0.05f; 
+    if (waveTime > 2 * PI) {
+        waveTime -= 2 * PI;
+    }
+    glutPostRedisplay(); 
+    glutTimerFunc(16, timer, 0); 
+}
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 
+    int numSlices = 100;
+    float flagWidth = 2.5f;
+    float sliceWidth = flagWidth / numSlices;
+
+    for (int i = 0; i < numSlices; i++) {
+        float xStart = -1.0f + i * sliceWidth;
+
+        float yOffset = sin(xStart * 3.0f + waveTime) * 0.05f;
+
+        glPushMatrix();
+        glTranslatef(0.0f, yOffset, 0.0f); 
+
+        glEnable(GL_SCISSOR_TEST);
+        int screenXStart = static_cast<int>(((xStart - (-1.0f)) / 2.5f) * 1000);
+        int screenSliceWidth = static_cast<int>((sliceWidth / 2.5f) * 1000) + 1;
+        glScissor(screenXStart, 0, screenSliceWidth, 600);
 
     drawRectangle(-1.0, -0.75, 2.5, 1.5, 0.0, 0.13, 0.45);
 
@@ -57,12 +84,15 @@ void display() {
 
     float centerX = 0.65;
     float ringRadius = 0.45;
-    for (int i = 0; i < 15; i++) {
-        float theta = i * 2.0 * PI / 15.0;
+    for (int j = 0; j < 15; j++) {
+        float theta = j * 2.0 * PI / 15.0;
         drawStar(centerX + ringRadius * cos(theta), ringRadius * sin(theta), 0.08, 0.035);
     }
 
-    glFlush();
+    glDisable(GL_SCISSOR_TEST);
+    glPopMatrix();
+}
+   glutSwapBuffers();
 }
 
 void init() {
@@ -76,13 +106,14 @@ void init() {
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 
 
     glutInitWindowSize(1000, 600);
     glutCreateWindow("Cook Islands Flag - Full Screen");
     init();
     glutDisplayFunc(display);
+    glutTimerFunc(0, timer, 0);
     glutMainLoop();
     return 0;
 }
